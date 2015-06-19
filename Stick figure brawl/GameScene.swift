25d78f26099d9c6b7675871071d066b16f318dd3
Jettbis <jettbis2002@gmail.com>
2015-06-19 15:25:54 -0400

    Initial commit of game

---
+++ Stick figure brawl/GameScene.swift
@@ -0,0 +1,253 @@
+//
+//  GameScene.swift
+//  Stick figure brawl
+//
+//  Created by Jarrett Bishop on 6/17/15.
+//  Copyright (c) 2015 Anti-Hero. All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    
+    var world = SKNode()
+    let hero = SKSpriteNode(imageNamed: "punch1")
+    var platform = SKNode()
+    let grunts = SKNode()
+   var herohealth = 50
+    var killedenemies = 0
+    let healthbar = SKLabelNode(text: "r")
+    let enemieskilled = SKLabelNode(text: "t")
+    let fall1 = SKAction.moveTo(CGPoint(x: 1025, y: 197), duration: 2)
+    let fall2 = SKAction.moveTo(CGPoint(x: 25, y: 197), duration: 2)
+    let fall3 = SKAction.moveTo(CGPoint(x: 1075, y: 197), duration: 2)
+    let fall4 = SKAction.moveTo(CGPoint(x: 50, y: 197), duration: 2)
+    
+    
+    
+    
+    
+    
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+       world = childNodeWithName("world")!
+        platform = world.childNodeWithName("platform")!
+        
+        
+        hero.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.234)
+        hero.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        
+        world.addChild(hero)
+        world.addChild(grunts)
+        addChild(healthbar)
+        addChild(enemieskilled)
+
+    
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+    }
+    
+    func punch() {
+        hero.removeActionForKey("running")
+        let atlas = SKTextureAtlas(named: "punching")
+        let textures = [
+            atlas.textureNamed("punch1"),
+            atlas.textureNamed("punch2"),
+            atlas.textureNamed("punch3"),
+            atlas.textureNamed("punch4"),
+            atlas.textureNamed("punch5"),
+            atlas.textureNamed("punch6"),
+            atlas.textureNamed("punch7"),
+        
+        ]
+        let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.05, resize: true, restore: false)
+        
+        hero.runAction(SKAction.sequence([actionAnim, SKAction.setTexture(SKTexture(imageNamed: "punch1"), resize: true)]))
+        
+        for grunt in grunts.children {
+            if hero.frame .intersects(grunt.frame) {
+                grunt.removeFromParent()
+                killedenemies = killedenemies+1
+                enemieskilled.text = ("ENEMIES KILLED \(killedenemies)")
+                enemieskilled.position = CGPoint(x: self.size.width*0.2, y: self.size.height*0.9)
+                enemieskilled.zPosition = 17
+            }
+        }
+    }
+
+    func runright() {
+        let atlas = SKTextureAtlas(named: "running")
+        let textures = [
+            atlas.textureNamed("running1"),
+            atlas.textureNamed("running2"),
+            atlas.textureNamed("running3"),
+            atlas.textureNamed("running4"),
+            atlas.textureNamed("running5"),
+            atlas.textureNamed("running6"),
+            atlas.textureNamed("running7"),
+            
+        ]
+        //let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.05)
+        let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.08, resize: true, restore: false)
+        
+        
+       
+        hero.runAction(SKAction.group([
+                SKAction.repeatActionForever(actionAnim),
+                SKAction.repeatActionForever(SKAction.moveBy(CGVector(dx: 500, dy: 0), duration: 1))
+            ]), withKey: "running")
+        hero.xScale = 1
+    }
+
+
+    func runleft() {
+        let atlas = SKTextureAtlas(named: "running")
+        let textures = [
+            atlas.textureNamed("running1"),
+            atlas.textureNamed("running2"),
+            atlas.textureNamed("running3"),
+            atlas.textureNamed("running4"),
+            atlas.textureNamed("running5"),
+            atlas.textureNamed("running6"),
+            atlas.textureNamed("running7"),
+            
+        ]
+        //let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.05)
+        let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.08, resize: true, restore: false)
+        
+        
+  
+        hero.runAction(SKAction.group([
+            SKAction.repeatActionForever(actionAnim),
+            SKAction.repeatActionForever(SKAction.moveBy(CGVector(dx: -500, dy: 0), duration: 1))
+            ]), withKey: "running")
+        hero.xScale = -1
+    }
+
+
+    override func didFinishUpdate() {
+       
+        
+    
+        world.position.x = -hero.position.x + self.size.width*0.5
+        world.position.y = -hero.position.y + self.size.height*0.2
+        if hero.frame .intersects(platform.frame) == false {
+            hero.runAction(SKAction.repeatActionForever(SKAction.moveBy(CGVector(dx: 0, dy: -500), duration: 2)))
+        }
+    
+        // GRUNT PUNCH SECTION
+        for grunt in grunts.children {
+            if grunt.frame .intersects(hero.frame) == true {
+                
+                if grunt.actionForKey("punch") == nil {
+                
+                    let atlas = SKTextureAtlas(named: "gruntpunch")
+                    let textures = [
+                        atlas.textureNamed("gruntpunch21"),
+                        atlas.textureNamed("gruntpunch22"),
+                        atlas.textureNamed("gruntpunch23"),
+                        atlas.textureNamed("gruntpunch24"),
+                        
+                    ]
+                    //let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.05)
+                    let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.08, resize: true, restore: false)
+                    grunt.runAction(SKAction.sequence([SKAction.waitForDuration(0.2), SKAction.runBlock({ () -> Void in
+                        self.herohealth = self.herohealth - 1
+                        self.healthbar.text = "\(self.herohealth) /50"
+                    }), actionAnim]), withKey: "punch")
+                   
+                    healthbar.position = CGPoint(x: self.size.width*0.87, y: self.size.height*0.9)
+                    healthbar.zPosition = 4
+                    if herohealth == 0 {
+                        hero.removeFromParent()
+                        healthbar.removeFromParent()
+                        showGameOver()
+                    }
+                }
+            }
+        }
+        for grunt in grunts.children {
+           
+            if grunt.actionForKey("punch") == nil {
+                if grunt.actionForKey("run") == nil {
+                    if grunt.actionForKey("fall") == nil {
+        
+                        let chase = SKAction.moveTo( hero.position, duration: 2)
+                        grunt.runAction(chase)
+        
+                    let atlas = SKTextureAtlas(named: "gruntrun")
+                    let textures = [
+                        atlas.textureNamed("gruntrun1"),
+                        atlas.textureNamed("gruntrun2"),
+                        atlas.textureNamed("gruntrun3"),
+                        atlas.textureNamed("gruntrun4"),
+                        atlas.textureNamed("gruntrun5"),
+                        atlas.textureNamed("gruntrun6"),
+                        atlas.textureNamed("gruntrun7"),
+                        atlas.textureNamed("gruntrun8"),
+                        
+                    ]
+                    let actionAnim = SKAction.animateWithTextures(textures, timePerFrame: 0.08, resize: true, restore: false)
+                    grunt.runAction(actionAnim, withKey: "run")
+                    }
+                }
+            }
+        }
+        // GRUNT SPAWNING
+        if grunts.children.count < 4 {
+
+            let grunt = SKSpriteNode(imageNamed: "gruntpunch21")
+            grunt.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+            grunts.addChild(grunt)
+             
+            
+            if grunts.children.count == 1 {
+                grunt.position = CGPoint(x: 1025, y: 1000)
+                grunt.runAction(fall1, withKey: "fall")
+                grunt.xScale = -1
+            
+            }
+            
+            
+            if grunts.children.count == 2{
+                grunt.position = CGPoint(x: 25, y: 1000)
+                grunt.runAction(fall2, withKey: "fall")
+            }
+            
+            if grunts.children.count == 3{
+                grunt.position = CGPoint(x: 925, y: 1000)
+                grunt.runAction(fall3, withKey: "fall")
+                grunt.xScale = -1
+          
+            
+            }
+            
+            if grunts.children.count == 4{
+                grunt.position = CGPoint(x: 100, y: 1000)
+                grunt.runAction(fall4, withKey: "fall")
+            }
+        }
+
+    }
+ 
+    func showGameOver() {
+        let backround = SKSpriteNode(imageNamed: "gameover")
+        backround.position = CGPoint(x: self.size.width*0.5, y: self.size.width*0.38)
+        backround.size = CGSize(width: self.size.width, height: self.size.height)
+        backround.zPosition = 16
+        addChild(backround)
+    }
+}
+
+
+
+
+
+
+
---
+++ Stick figure brawl/GameViewController.swift
@@ -0,0 +1,75 @@
+//
+//  GameViewController.swift
+//  Stick figure brawl
+//
+//  Created by Jarrett Bishop on 6/17/15.
+//  Copyright (c) 2015 Anti-Hero. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : String) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
+
+class GameViewController: UIViewController {
+    var scene = GameScene.unarchiveFromFile("GameScene") as! GameScene
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .AspectFill
+        
+        skView.presentScene(scene)
+    }
+
+    @IBAction func punchButtonPressed(sender: UIButton) {
+        scene.punch()
+    }
+    
+    @IBAction func rightButtonPressed(sender: UIButton) {
+        scene.runright()
+    }
+    
+    @IBAction func leftButtonPressed(sender: UIButton) {
+        scene.runleft()
+    }
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.Landscape.rawValue)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
